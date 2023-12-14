7c47ef2f4d9121fe1f755725a52d18b2b85dca0f
fernanda de leon <fernanda.-deleon@hotmail.com>
2023-12-14 21:53:03 +0100

    Add iterative power calulation

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import numpy as np
+from scipy.sparse import csc_matrix
+
+def page_rank(pi0, H, n, alpha, epsilon):
+    row_sum_vector = np.sum(H, axis=1).A1  # Get row sums for H
+    non_zero_rows = np.where(row_sum_vector != 0)[0]
+    zero_rows = np.where(row_sum_vector == 0)[0]
+
+    a = np.zeros(n)
+    a[zero_rows] = 1
+
+    k = 0
+    residual = 1
+    pi = pi0.copy()
+
+    while residual >= epsilon:
+        prev_pi = pi.copy()
+        k += 1
+        pi = alpha * pi.dot(H) + (alpha * (pi.dot(a) + 1 - alpha)) * (np.ones(n) / n)
+        residual = np.linalg.norm(pi - prev_pi, ord=1)
+
+    return pi, k
+
+# Example usage
 
 #Dictionay
 links = {
@@ -15,25 +38,25 @@ links = {
 }
 
 # Find the maximum page number from the links
-max_page = max(links.keys())
+n = max(links.keys())
 
-# Create a matrix H filled with zeros of size (max_page x max_page)
-H = np.zeros((max_page, max_page))
+# Create a matrix H filled with zeros of size (n x n)
+H = np.zeros((n, n))
 
 # Fill the matrix H based on links information
 for i, outbound_links in links.items():
     for j in outbound_links:
         H[i - 1][j - 1] = round(1 / len(outbound_links), 3)
+H = H / H.sum(axis=1)[:, np.newaxis]  # Normalize rows to ensure it is row-stochastic
 
+pi0 = np.ones(n) / n  # Starting vector (usually set to the uniform vector)
+alpha = 0.9  # Scaling parameter
+epsilon = 1e-8  # Convergence tolerance
 
-print(H)
+# Convert H to a sparse matrix for efficiency
+H_sparse = csc_matrix(H)
 
-# Number of pages
-num_pages = len(links)
-
-# Create a matrix r filled with 1/num_pages
-r = [[1 / num_pages] * num_pages for _ in range(num_pages)]
-
-# Display the matrix r
-for row in r:
-    print(row)
+# Run the PageRank algorithm
+pagerank_vector, iterations = page_rank(pi0, H_sparse, n, alpha, epsilon)
+print("PageRank vector:", pagerank_vector)
+print("Number of iterations:", iterations)
