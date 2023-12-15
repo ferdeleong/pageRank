51c31fe4110045ab1802e0687e7b05b545022311
fernanda de leon <fernanda.-deleon@hotmail.com>
2023-12-15 23:55:35 +0100

    Add web graph, H hyperlink matrix

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
 import numpy as np
 from scipy.sparse import csc_matrix
 
-def page_rank(pi0, H, n, alpha, epsilon):
-    row_sum_vector = np.sum(H, axis=1).A1  # Get row sums for H
-    non_zero_rows = np.where(row_sum_vector != 0)[0]
-    zero_rows = np.where(row_sum_vector == 0)[0]
-
-    a = np.zeros(n)
-    a[zero_rows] = 1
-
-    k = 0
-    residual = 1
-    pi = pi0.copy()
-
-    while residual >= epsilon:
-        prev_pi = pi.copy()
-        k += 1
-        pi = alpha * pi.dot(H) + (alpha * (pi.dot(a) + 1 - alpha)) * (np.ones(n) / n)
-        residual = np.linalg.norm(pi - prev_pi, ord=1)
-
-    return pi, k
-
-# Example usage
+def print_matrix(matrix):
+  #Funtion to print matrixes, we'll use it throughout
+  for row in matrix:
+    print(row)
 
 #Dictionay
 links = {
@@ -34,7 +17,7 @@ links = {
     7: [1, 4, 6, 9],
     8: [2,3,7,8],
     9: [1, 3, 5, 7],
-    10: [2, 4, 6, 9],
+    10: [],
 }
 
 # Find the maximum page number from the links
@@ -47,16 +30,5 @@ H = np.zeros((n, n))
 for i, outbound_links in links.items():
     for j in outbound_links:
         H[i - 1][j - 1] = round(1 / len(outbound_links), 3)
-H = H / H.sum(axis=1)[:, np.newaxis]  # Normalize rows to ensure it is row-stochastic
-
-pi0 = np.ones(n) / n  # Starting vector (usually set to the uniform vector)
-alpha = 0.9  # Scaling parameter
-epsilon = 1e-8  # Convergence tolerance
-
-# Convert H to a sparse matrix for efficiency
-H_sparse = csc_matrix(H)
-
-# Run the PageRank algorithm
-pagerank_vector, iterations = page_rank(pi0, H_sparse, n, alpha, epsilon)
-print("PageRank vector:", pagerank_vector)
-print("Number of iterations:", iterations)
+        
+print_matrix(H)
